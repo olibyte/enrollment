cc5f7db90ddb8644ca64b2c7c0db75af2bf523bf
Oliver <ocben1@gmail.com>
2020-01-09 07:49:10 +1100

    create data models

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,6 @@
 from application import app, db
 from flask import render_template, request, json, Response
+from application.models import User, Course, Enrollment
 
 courseData = [{"courseID":"1111","title":"PHP 111","description":"Intro to PHP","credits":"3","term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":"4","term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":"3","term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":"3","term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":"4","term":"Fall"}]
 
@@ -38,13 +39,6 @@ def api(idx=None):
         jdata = courseData[int(idx)]
     
     return Response(json.dumps(jdata), mimetype="application/json")
-
-class User(db.Document):
-    user_id     =   db.IntField( unique=True )
-    first_name  =   db.StringField( max_length=50 )
-    last_name   =   db.StringField( max_length=50 )
-    email       =   db.StringField( max_length=30 )
-    password    =   db.StringField( max_length=30 )
 
 @app.route("/user")
 def user():
