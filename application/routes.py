47f945d6e8154b6b0b68ee17c9b012690a4a75a1
Oliver <ocben1@gmail.com>
2020-01-17 07:10:45 +1100

    flashing messages

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,6 @@
 from application import app, db
-from flask import render_template, request, json, Response
-from application.models import User, Course
+from flask import render_template, request, json, Response, redirect, flash
+from application.models import User, Course, Enrollment
 from application.forms import LoginForm, RegisterForm
 
 courseData = [{"courseID":"1111","title":"PHP 111","description":"Intro to PHP","credits":"3","term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":"4","term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":"3","term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":"3","term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":"4","term":"Fall"}]
@@ -14,6 +14,12 @@ def index():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if request.form.get("email") == "test@uta.com":
+            flash("You are successfully logged in!", "success")
+            return redirect("/index")
+        else:
+            flash("Sorry, something went wrong.", "danger")
     return render_template("login.html", title="Login", form=form, login=True )
 
 @app.route("/courses/")
