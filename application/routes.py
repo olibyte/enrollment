b9b248672ca68b8295072186aef6d1d94338c1b0
Oliver <ocben1@gmail.com>
2020-01-17 06:51:04 +1100

    update login route and login template

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,7 @@
 from application import app, db
 from flask import render_template, request, json, Response
-from application.models import User, Course, Enrollment
+from application.models import User, Course
+from application.forms import LoginForm, RegisterForm
 
 courseData = [{"courseID":"1111","title":"PHP 111","description":"Intro to PHP","credits":"3","term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":"4","term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":"3","term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":"3","term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":"4","term":"Fall"}]
 
@@ -10,9 +11,10 @@ courseData = [{"courseID":"1111","title":"PHP 111","description":"Intro to PHP",
 def index():
     return render_template("index.html", index=True )
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html", login=True )
+    form = LoginForm()
+    return render_template("login.html", title="Login", form=form, login=True )
 
 @app.route("/courses/")
 @app.route("/courses/<term>")
