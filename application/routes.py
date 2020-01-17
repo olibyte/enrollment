95e2419ec0aba62e4730ed7e778e96cfaaa4d8cc
Oliver <ocben1@gmail.com>
2020-01-18 09:04:37 +1100

    updating login route to interact with database

--- application/routes.py
+++ application/routes.py
@@ -15,8 +15,13 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if request.form.get("email") == "test@uta.com":
-            flash("You are successfully logged in!", "success")
+        email = form.email.data
+        password = form.password.data
+
+
+        user = User.objects(email=email).first()
+        if user and user.get_password(password):
+            flash(f"{user.first_name}, you are successfully logged in!", "success")
             return redirect("/index")
         else:
             flash("Sorry, something went wrong.","danger")
